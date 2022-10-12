14fbec70253c34dfd09f5080ac460b7b5442fe4b
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-10-12 10:55:08 +0600

    Add blog pages wuestion and answer

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,57 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="bg-white">
+      <div
+        tabIndex={0}
+        className="collapse collapse-open border border-base-300 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          What is the purpose of react router?
+        </div>
+        <div className="collapse-content">
+          <p>
+            React Router is a standard library for routing in React. It enables
+            the navigation among views of various components in a React
+            Application, allows changing the browser URL, and keeps the UI in
+            sync with the URL.
+          </p>
+        </div>
+      </div>
+
+      <div
+        tabIndex={1}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          What is the purpose of react router?
+        </div>
+        <div className="collapse-content">
+          <p>
+            React Router is a standard library for routing in React. It enables
+            the navigation among views of various components in a React
+            Application, allows changing the browser URL, and keeps the UI in
+            sync with the URL.
+          </p>
+        </div>
+      </div>
+
+      <div
+        tabIndex={2}
+        className="collapse collapse-open border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          What is the purpose of react router?
+        </div>
+        <div className="collapse-content">
+          <p>
+            React Router is a standard library for routing in React. It enables
+            the navigation among views of various components in a React
+            Application, allows changing the browser URL, and keeps the UI in
+            sync with the URL.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SingleQuiz = ({ quizz }) => {
   const { name, logo, id, question, options } = quizz;
+
   return (
     <div>
       <div>
